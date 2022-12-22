cae500a03c54dade3b1dd50c52417b9f8ee5ddeb
EsteemDev <117539167+EsteemDev@users.noreply.github.com>
2022-12-22 13:51:18 +0330

    Add files via upload

---
+++ src/js/script.js
@@ -0,0 +1,65 @@
+// 1- var : 1995 - 2015
+// 2- let : 2015 => ES6
+// 3- const : 2015 => ES6
+// 4- nothing
+
+// Initialize variable
+// keyword name; 
+// keyword name = value;
+// Set variable value
+// name = value;
+
+let test = "test";
+// let test = "test"; // Error
+
+var test1 = "test1";
+console.log(test1); // Result = test1
+var test1 = "test2"; // test1 = "test2";
+console.log(test1); // Result = "test2"
+
+// Case Sensitive
+let name;
+let Name;
+let NAME;
+
+// a-z A-Z 0-9 _ $
+let T$est25_;
+
+// Variable Name Should Start with a-z A-Z _ $
+let _;
+let _name;
+let $;
+let $name;
+let Test1;
+// let 125Test; // Error
+
+// let if; // Error
+// let while; //Error
+// let let; // Error
+// // ...
+
+console.log(test2); // Result = undefined
+var test2;
+
+// test3 = "test";
+// console.log(test3);
+// let test3; // Error in Browser
+
+// test4 = "const";
+// const test4; // Error
+
+test5 = "test5"; // any
+var test6 = ""; // type : string
+let test7 = 5; // type : number
+
+const AppName = "Application1";
+// AppName = "App2"; // Error in Browser
+
+let test8;
+console.log(test8); // result = undefined
+test8 = "test8";
+console.log(test8); // result = test8
+
+// const test9;
+// test9 = "test"; // Error
+
